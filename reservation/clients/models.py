871e8450c49cb770aa0a7ee95e60c5beeac73768
myhas <shahad.ahmed.t@outlook.com>
2024-05-07 19:44:45 +0300

    v1

--- reservation/clients/models.py
+++ reservation/clients/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+class ClientTypes(models.Model):
+    type = models.IntegerField(primary_key=True, max_length=5,default=0)
+    name = models.CharField(max_length=150, null=False)
+    description = models.CharField(max_length=500)
+def __str__(self):
+    return self.name
+class Client(models.Model):
+    gender_choices = [(1, "Male"), (2, "Female")]
+    cid = models.IntegerField(primary_key=True, max_length=5,default=0)
+    name = models.CharField(max_length=150, null=False)
+    gender = models.IntegerField(choices=gender_choices)
+    email= models.EmailField(unique=True)
+    birthdate = models.DateField()
+    type= models.ForeignKey(ClientTypes,on_delete=models.CASCADE, default=0)
+
+
+class Product(models.Model):
+    pid = models.IntegerField(primary_key=True, max_length=5,default=0)
+    name = models.CharField(max_length=150, null=False)
+    description = models.CharField(max_length=500)
+def __str__(self):
+    return self.name
+class Order(models.Model):
+    oid = models.IntegerField(primary_key=True, max_length=5,default=0)
+    cid= models.ForeignKey(Client,on_delete=models.CASCADE)
+    pid= models.ForeignKey(Product,on_delete=models.CASCADE) #could be manytomany
+    orderdate = models.DateField()
+    shippingdate = models.DateField()
+
+class ClientTypes(models.Model):
+    type = models.IntegerField(primary_key=True, max_length=5,default=0)
+    name = models.CharField(max_length=150, null=False)
+    description = models.CharField(max_length=500)
+def __str__(self):
+    return self.name
+class Client(models.Model):
+    gender_choices = [(1, "Male"), (2, "Female")]
+    cid = models.IntegerField(primary_key=True, max_length=5,default=0)
+    name = models.CharField(max_length=150, null=False)
+    gender = models.IntegerField(choices=gender_choices)
+    email= models.EmailField(unique=True)
+    birthdate = models.DateField()
+    type= models.ForeignKey(ClientTypes,on_delete=models.CASCADE, default=0)
+
+
+
+class Product(models.Model):
+    pid = models.IntegerField(primary_key=True, max_length=5,default=0)
+    name = models.CharField(max_length=150, null=False)
+    description = models.CharField(max_length=500)
+def __str__(self):
+    return self.name
+class Order(models.Model):
+    oid = models.IntegerField(primary_key=True, max_length=5,default=0)
+    cid= models.ForeignKey(Client,on_delete=models.CASCADE)
+    pid= models.ForeignKey(Product,on_delete=models.CASCADE) #could be manytomany
+    orderdate = models.DateField()
+    shippingdate = models.DateField()
